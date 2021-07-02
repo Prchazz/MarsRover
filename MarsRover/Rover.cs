5b8a6216e76aec8fcf9fd79886052b9bdb3db0c2
Charlie <Charlie@carde-macbook.myfiosgateway.com>
2021-07-02 11:20:13 -0400

    first commit after clone repo

---
+++ MarsRover/Rover.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace MarsRover
+{
+    public class Rover
+    {
+        public string Mode { get; set; }
+        public int Position { get; set; }
+        public int GeneratorWatts { get; set; }
+
+        public Rover(int position)
+        {
+        }
+
+        public override string ToString()
+        {
+            return "Position: " + Position + " - Mode: " + Mode + " - GeneratorWatts: " + GeneratorWatts; 
+        }
+
+    }
+}
---
+++ MarsRoverTests/CommandTests.cs
@@ -0,0 +1,39 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MarsRover;
+using System;
+
+namespace MarsRoverTests
+{
+    [TestClass]
+    public class CommandTests
+    {
+
+        [TestMethod]
+        public void ArgumentNullExceptionThrownIfCommandTypeIsNullOrEmpty()
+        {
+            try
+            {
+                new Command("");
+            }
+            catch (ArgumentNullException ex)
+            {
+                Assert.AreEqual("Command type required.", ex.Message);
+            }
+        }
+
+        [TestMethod]
+        public void ConstructorSetsCommandType()
+        {
+            Command newCommand = new Command("MOVE", 0);
+            Assert.AreEqual(newCommand.CommandType, "MOVE");
+        }
+
+        [TestMethod]
+        public void ConstructorSetsInitialNewPositionValue()
+        {
+            Command newCommand = new Command("MOVE", 20);
+            Assert.AreEqual(newCommand.NewPostion, 20);
+        }
+
+    }
+}
